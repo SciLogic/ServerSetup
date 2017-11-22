800b90ff6693e0011dcf71346e72e30566d41ed6
Zachary Smith <Scilogic@protonmail.com>
2017-11-22 11:35:32 +0000

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -14,23 +14,23 @@ Cyan='\033[0;36m'	# Cyan
 Color_Off='\033[0m'	#Reset Text Colour
 
 echo  "$Green \n Checking & installing updates.... $Color_Off"
-yes | sudo apt-get update -y >/dev/null && sudo apt-get upgrade >/dev/null && echo "$Green [OK] $Color_Off"
+yes | sudo apt-get update -y 2>&-/dev/null && sudo apt-get upgrade 2>&-/dev/null && echo "$Green [OK] $Color_Off"
 
 ## Install AMP
 echo "$Green \n Installing Apache2... $Color_Off"
-yes | sudo apt-get install apache2 apache2-doc apache2-utils apache2-dev libexpat1 ssl-cert -y >/dev/null && echo "$Green [OK] $Color_Off"
+yes | sudo apt-get install apache2 apache2-doc apache2-utils apache2-dev libexpat1 ssl-cert -y 2>&-/dev/null && echo "$Green [OK] $Color_Off"
 
 echo "$Green \n Installing PHP & Reqs.... $Color_Off"
-yes | sudo apt-get install libapache2-mod-php7.0 php7.0 php7.0-common php7.0-curl php7.0-dev php7.0-gd php-pear php-imagick php7.0-mcrypt php7.0-mysql >/dev/null && echo "$Green [OK] $Color_Off"
+yes | sudo apt-get install libapache2-mod-php7.0 php7.0 php7.0-common php7.0-curl php7.0-dev php7.0-gd php-pear php-imagick php7.0-mcrypt php7.0-mysql 2>&-/dev/null && echo "$Green [OK] $Color_Off"
 
 echo "$Green \n Installing MySQL.... $Color_Off"
-yes | sudo apt-get install mysql-server mysql-client -y >/dev/null && echo "$Green [OK] $Color_Off"
+yes | sudo apt-get install mysql-server mysql-client -y 2>&-/dev/null && echo "$Green [OK] $Color_Off"
 
 echo "$Green \n Installing phpMyAdmin....$Color_Off"
-yes | sudo apt-get install phpmyadmin -y >/dev/null && echo "$Green [OK] $Color_Off"
+yes | sudo apt-get install phpmyadmin -y 2>&-/dev/null && echo "$Green [OK] $Color_Off"
 
 echo "$Green \n Verifying installs.... $Color_Off"
-yes | sudo apt-get install apache2 libapache2-mod-php7.0 php7.0 mysql-server php-pear php7.0-mysql mysql-client mysql-server php-mysql php7.0-gd -y >/dev/null && echo "$Green [OK] $Color_Off"
+yes | sudo apt-get install apache2 libapache2-mod-php7.0 php7.0 mysql-server php-pear php7.0-mysql mysql-client mysql-server php-mysql php7.0-gd -y 2>&-/dev/null && echo "$Green [OK] $Color_Off"
 
 
 ##Tweaks & Settings
